453af2574be479f4dea96c2ad158e1183f98313e
priscila-siqueira <priscilasds1997@gmail.com>
2025-08-20 21:19:49 -0300

    subindo nova versão

--- script.js
+++ script.js
@@ -44,11 +44,17 @@ const BotaoConclui = () =>{
     botaoConclui.classList.add('check-button');
     botaoConclui.innerText = 'concluir'
 
-    botaoConclui.addEventListener('click', () => {
-        console.log('Fui clicado!!!');
-
-    });
+    botaoConclui.addEventListener('click', concluirTarefa);
     return botaoConclui;
+};
+
+
+const concluirTarefa = (evento) => {
+    const eventoAcionado = evento.target 
+
+    const tarefaCompleta = eventoAcionado.parentElement
+
+    tarefaCompleta.classList.toggle('done'); 
 };
 
 
